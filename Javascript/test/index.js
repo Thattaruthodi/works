a4eb14e5f7d8db8781e8ba690c5b5f5a234cf08e
Thattaruthodi <fathimafemina5030@gmail.com>
2023-01-30 17:02:12 +0530

    fsdfd

---
+++ Javascript/test/index.js
@@ -0,0 +1,61 @@
+const b = 5;
+
+var c = 9999999999999999999999999999999999999999999999999999999999999;
+d = false;
+let e;
+c = "esh";
+e = {
+  name: "Femina",
+  age: 36,
+};
+
+let f = ["femina", 36, 5, 88, 6];
+// console.log(f.length);
+
+const date = new Date();
+let a = 5;
+
+console.log({ c });
+console.log(a);
+console.log(a);
+
+if (a === 5) {
+  const g = "Hello";
+  let k = 5;
+  var j = 98;
+
+  console.log({
+    g,
+    k,
+    j,
+    b,
+  });
+}
+
+let n = 610;
+let m = 686;
+let v = 79;
+// n = n + 1
+
+// if (n > m) {
+//   document.getElementById("demo").innerHTML = "Hello";
+// } else if (n < m) {
+//   document.getElementById("demo").innerHTML = "World";
+// } else {
+//   document.getElementById("demo").innerHTML = "Man";
+// }
+
+// function sum() {
+//   //   alert("Hello");
+//   document.getElementById("demo2").innerHTML = "Hello";
+// }
+
+function sum(a, b) {
+  document.getElementById("demo2").innerHTML = a + b;
+}
+
+// n >= m
+//   ? (document.getElementById("demo").innerHTML = "Hello")
+//   : (document.getElementById("demo").innerHTML = "World");
+
+console.log();
